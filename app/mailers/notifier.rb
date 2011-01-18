896bce6d64440e0801b0a9bbddfe3238412295ab
Finne Jager <finne@ciagent.com>
2011-01-18 14:14:14 -0500

    mailer2

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,5 +1,5 @@
 class Notifier < ActionMailer::Base
-  default :from => "finne@phynn.com"
+  default :from => "finne@ciagent.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
