082243e11a2dcca4bc9924d1c2cac41d41c1ad5b
Finne Jager <finne@ciagent.com>
2011-02-25 10:48:05 -0500

    mandatory poliy number

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -3,6 +3,8 @@ class Incident < ActiveRecord::Base
 	has_one :timesheet, :dependent => :destroy
 	has_one :mat_list, :dependent => :destroy
 	
+	validates :policy_nr, :presence => true
+	
 	def truncated_time
 		incident_datetime.strftime("%m-%d-%Y")
 	end
